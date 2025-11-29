4b8af2ca3f85da8ed6b23996cf464b97dd47c0f7
Dmoreno2903 <diegomoreno29003@gmail.com>
2025-11-29 12:19:29 -0500

    Implement core functionality for word counting in wordcount.py

---
+++ homework/src/_internals/__init__.py
@@ -0,0 +1,13 @@
+from .count_words import count_words
+from .split_into_words import split_into_words
+from .write_word_counts import write_word_counts
+from .preprocess_lines import preprocess_lines
+from .read_all_lines import read_all_lines
+
+__all__ = [
+    count_words,
+    split_into_words,
+    write_word_counts,
+    preprocess_lines,
+    read_all_lines,
+]
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,20 @@
+from homework.src._internals import count_words, split_into_words, count_words, write_word_counts, preprocess_lines, read_all_lines
 
+def main():
+    """Count the frequency of the words in the files in the input directory
+    """
+    import sys
+    import os
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_dir> <output_file>")
+        sys.exit(1)
+
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    all_lines = read_all_lines(input_dir)
+    preprocessed_lines = preprocess_lines(all_lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder=output_dir, word_counts=word_counts)
